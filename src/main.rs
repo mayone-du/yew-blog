1892494dc0e35d1133e9050ae23c8a1f51a2766f
mayone-du <cocomayo1201@icloud.com>
2021-12-26 22:25:24 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod layouts;
 mod pages;
 mod routes;
 
+use routes::app_routes::AppRoutes;
+
 enum Msg {
   AddOne,
 }
@@ -38,13 +40,12 @@ impl Component for Model {
     let link = ctx.link();
     html! {
       <BrowserRouter>
-      <Switch<routes::app_routes::AppRoutes> render={Switch::render(switch)} />
-
+      <Switch<AppRoutes> render={Switch::render(switch)} />
         <div>
           <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
           <p>{ self.value }</p>
           <components::button::Button />
-          <Link<routes::app_routes::AppRoutes> to={routes::app_routes::AppRoutes::Home}>{"go home"}</Link<routes::app_routes::AppRoutes>>
+          <Link<AppRoutes> to={AppRoutes::Home}>{"go home"}</Link<AppRoutes>>
         </div>
       </BrowserRouter>
     }
@@ -56,19 +57,19 @@ fn main() {
 }
 
 // TODO: 同じパスへ遷移したときは再レンダリングとかhistory pushはいらない？
-fn switch(routes: &routes::app_routes::AppRoutes) -> Html {
+fn switch(routes: &AppRoutes) -> Html {
   match routes {
-    routes::app_routes::AppRoutes::Home => html! { <pages::index::IndexPage /> },
-    routes::app_routes::AppRoutes::Profile => html! { <pages::profile::ProfilePage /> },
-    routes::app_routes::AppRoutes::NotFound => html! { <h1>{ "404" }</h1> },
+    AppRoutes::Home => html! { <pages::index::IndexPage /> },
+    AppRoutes::Profile => html! { <pages::profile::ProfilePage /> },
+    AppRoutes::NotFound => html! { <h1>{ "404" }</h1> },
   }
 }
 
 #[function_component(Main)]
 fn app() -> Html {
   html! {
-      <BrowserRouter>
-          <Switch<routes::app_routes::AppRoutes> render={Switch::render(switch)} />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Switch<routes::app_routes::AppRoutes> render={Switch::render(switch)} />
+    </BrowserRouter>
   }
 }
