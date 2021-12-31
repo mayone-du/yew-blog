6e2d3c20ec48c0125a8fd99fe5c593f84e057ccf
mayone-du <mayone-du@users.noreply.github.com>
2021-12-31 08:45:37 +0000

    deploy: 6e2c515d3aafdd47c8465de33fdee20347890850

--- index-fdd1346a1ef755d2.js
+++ index-fdd1346a1ef755d2.js
@@ -314,7 +314,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d512d899299593ec_bg.wasm', import.meta.url);
+        input = new URL('index-fdd1346a1ef755d2_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
