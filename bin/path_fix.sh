1ea96e3e28c2c76de4936db3671e46110b3e6c5d
mayone-du <cocomayo1201@icloud.com>
2022-01-01 18:18:44 +0900

    fix

--- bin/path_fix.sh
+++ bin/path_fix.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -e 's/"\//".\//g' -e "s/'\//'.\//g" ./dist/index.html > ./dist/index.html
+cat ./dist/index.html | sed -e 's/"\//".\//g' -e "s/'\//'.\//g" >> ./dist/fixed.html && mv ./dist/fixed.html ./dist/index.html
