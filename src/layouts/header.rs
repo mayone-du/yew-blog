0fc8d77911d09ba06aec36719aa12cdfe84afc2d
mayone-du <cocomayo1201@icloud.com>
2022-01-16 18:52:51 +0900

    add: links

--- src/layouts/header.rs
+++ src/layouts/header.rs
@@ -22,12 +22,15 @@ impl Component for Header {
           <Link<AppRoutes> classes="text-2xl font-bold" to={AppRoutes::Home}>{"mayoblog"}</Link<AppRoutes>>
         </div>
         <nav>
-          <ul class="grid grid-cols-2 gap-4">
+          <ul class="grid grid-cols-3 gap-4">
             <li class="col-span-1">
               <Link<AppRoutes> classes="block text-blue-500 underline transition-all hover:no-underline" to={AppRoutes::Home}>{"Home"}</Link<AppRoutes>>
             </li>
             <li class="col-span-1">
               <Link<AppRoutes> classes="block text-blue-500 underline transition-all hover:no-underline" to={AppRoutes::Profile}>{"Profile"}</Link<AppRoutes>>
+            </li>
+            <li class="col-span-1">
+              <a href="https://zenn.dev/mayo_dev" target="_blank" rel="noopener noreferrer" class="block text-blue-500 underline transition-all hover:no-underline">{"TechBlog"}</a>
             </li>
           </ul>
         </nav>
